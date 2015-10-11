326fb9c13c8ce086445a61f268916ad0d4ed42f7
Lyndon <lyndon@ksbartlett.com>
2015-10-10 20:46:49 -0500

    BlockstagramCheckpoint27AssignmentComplete
    Complete

--- ImagesTableViewController.m
+++ ImagesTableViewController.m
@@ -49,7 +49,7 @@
 #pragma mark - Table view data source
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-#warning Incomplete implementation, return the number of rows
+//#warning Incomplete implementation, return the number of rows
     return self.images.count;
 }
 
@@ -82,25 +82,30 @@
     return (CGRectGetWidth(self.view.frame) / image.size.width) * image.size.height;
 }
 
-/*
-// Override to support conditional editing of the table view.
+
 - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the specified item to be editable.
+ 
     return YES;
 }
-*/
 
-/*
+
+
 // Override to support editing the table view.
 - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
     if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
+        [self.images removeObjectAtIndex:indexPath.row];
         [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
+        
+        [tableView reloadData];
+        
+//    } else {
+//        NSLog(@"Not implementated yet! %ld", editingStyle);
+    }
+
+
+    }
+
+
 
 /*
 // Override to support rearranging the table view.
