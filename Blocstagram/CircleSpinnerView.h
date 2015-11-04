2b36128d22819ed5157928db5894630972dd9e7f
Lyndon <lyndon@ksbartlett.com>
2015-11-04 12:21:33 -0600

    BlocstagramCheckpoint39Complete
    Complete

---
+++ Blocstagram/CircleSpinnerView.h
@@ -0,0 +1,17 @@
+//
+//  CircleSpinnerView.h
+//  Blocstagram
+//
+//  Created by Lyndon Bartlett on 11/4/15.
+//  Copyright © 2015 Lyndon Bartlett. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CircleSpinnerView : UIView
+
+@property (nonatomic, assign) CGFloat strokeThickness;
+@property (nonatomic, assign) CGFloat radius;
+@property (nonatomic, strong) UIColor *strokeColor;
+
+@end
--- Blocstagram/DataSource.h
+++ Blocstagram/DataSource.h
@@ -23,7 +23,7 @@ typedef void (^NewItemCompletionBlock)(NSError *error);
 - (void) requestNewItemsWithCompletionHandler:(NewItemCompletionBlock)completionHandler;
 - (void) requestOldItemsWithCompletionHandler:(NewItemCompletionBlock)completionHandler;
 - (void) downloadImageForMediaItem:(Media *)mediaItem;
- - (void) downloadImageForMediaItem:(Media *)mediaItem;
+- (void) toggleLikeOnMediaItem:(Media *)mediaItem withCompletionHandler:(void (^)(void))completionHandler;
 
 @property (nonatomic, strong, readonly) NSString *accessToken;
 
