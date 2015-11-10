39284e430da8d6cd4b6ec5a479251e05e456e41d
Lyndon <lyndon@ksbartlett.com>
2015-11-09 21:23:21 -0600

    BlocstagramCheckpoint43Complete
    Complete

---
+++ Blocstagram/PostToInstagramViewController.h
@@ -0,0 +1,15 @@
+//
+//  PostToInstagramViewController.h
+//  Blocstagram
+//
+//  Created by Lyndon Bartlett on 11/9/15.
+//  Copyright © 2015 Lyndon Bartlett. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PostToInstagramViewController : UIViewController
+
+- (instancetype) initWithImage:(UIImage *)sourceImage;
+
+@end
